2d87138f2948a82b7beebe1d7f3e512956649329
haegnim <84562770+Haegnim@users.noreply.github.com>
2023-06-07 19:35:40 +0900

    0607 수정&삭제 추가

--- static/js/data.js
+++ static/js/data.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('#choose .update-input-box').empty();
     listing();
 });
 
@@ -9,17 +10,30 @@ function listing() {
             let rows = data['result'];
             $('#comments-box').empty();
             rows.forEach((a) => {
-                let num = a['id'];
+                let id = a['id'];
                 let name = a['name'];
                 let comment = a['comment'];
                 let date = a['date'];
 
                 let temp_html = `
                                 <tr>
-                                    <td>${num}</td>
+                                    <td>${id}</td>
                                     <td>${name}</td>
-                                    <td>${comment}</td>
+                                    <td class="txt-box bf">${comment}</td>
+                                    <td class="update-input-box af">
+                                    </td>
+
                                     <td>${date}</td>
+                                    <td class="btn-box btn-before bf">
+                                        <button onclick="update_btn(this)" class="btn-click">수정</button>
+                                        <button onclick="delete_btn(${id})" class="btn-click">삭제</button>
+                                    </td>
+                                    <td class="btn-box btn-after af">
+                                        <button onclick="update_scc(${id})" class="btn-click">수정 완료</button>
+                                        <button onclick="update_cancle()" class="btn-click">
+                                            수정 취소
+                                        </button>
+                                    </td>
                                 </tr>
                             `;
                 $('#comments-box').append(temp_html);
@@ -35,6 +49,18 @@ function save_comment() {
     formData.append('name_give', name);
     formData.append('comment_give', comment);
     fetch('/guestbook', { method: 'POST', body: formData })
+        .then((response) => response.json())
+        .then((data) => {
+            alert(data['msg']);
+            window.location.reload();
+        });
+}
+
+function delete_btn(a) {
+    let num = a;
+    let formData = new FormData();
+    formData.append('id_give', num);
+    fetch('/delete', { method: 'POST', body: formData })
         .then((response) => response.json())
         .then((data) => {
             alert(data['msg']);
